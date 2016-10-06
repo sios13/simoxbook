ff3e176bf5f4d366660310dbc2b7ca9a005a4e15
sios13 <simon2@live.se>
2016-10-06 11:51:46 +0200

    format changes to Elements.php

--- app/library/Elements.php
+++ app/library/Elements.php
@@ -1,24 +1,22 @@
 <?php
 
-use Simox\Component;
-
-class Elements extends Component
+class Elements extends Simox\Component
 {
     public function getMenu()
     {
         $menu = "<ul class='menu'>";
-        $menu .= "<li class='menu__item'><a href=".$this->url->get( "forum" ).">Forum</a></li>";
-        $menu .= "<li class='menu__item'><a href=".$this->url->get( "poll" ).">Omröstning</a></li>";
+        $menu .= "<li class='menu__item'><a href=". $this->url->get( "forum" ) .">Forum</a></li>";
+        $menu .= "<li class='menu__item'><a href=". $this->url->get( "poll" ) .">Omröstning</a></li>";
         
         if ( $this->session->get("auth") )
         {
-        $menu .= "<li class='menu__item'><a href=".$this->url->get( "profile" ).">Profil</a></li>";
-            $menu .= "<li class='menu__item'><a href=".$this->url->get( "logout" ).">Logga ut</a></li>";
+            $menu .= "<li class='menu__item'><a href=". $this->url->get( "profile" ) .">Profil</a></li>";
+            $menu .= "<li class='menu__item'><a href=". $this->url->get( "logout" ) .">Logga ut</a></li>";
         }
         else
         {
-        $menu .= "<li class='menu__item'><a href=".$this->url->get( "create" ).">Skapa ett konto</a></li>";
-            $menu .= "<li class='menu__item'><a href=".$this->url->get( "login" ).">Logga in</a></li>";
+            $menu .= "<li class='menu__item'><a href=". $this->url->get( "create" ) .">Skapa ett konto</a></li>";
+            $menu .= "<li class='menu__item'><a href=". $this->url->get( "login" ) .">Logga in</a></li>";
         }
         
         $menu .= "</ul>";
