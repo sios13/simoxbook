dd65381d7061c8d46159568155adc428f56d1536
sios13 <simon2@live.se>
2017-01-28 11:41:55 +0100

    added addAction to PollController

--- app/controllers/PollController.php
+++ app/controllers/PollController.php
@@ -5,50 +5,71 @@ class PollController extends ControllerBase
     public function initialize()
     {
         parent::initialize();
+
+        $this->tag->setTitle( "Omröstning" );
     }
-    
+
     public function indexAction()
     {
-        $this->tag->setTitle( "Omröstning" );
         $this->view->pick( "poll/index" );
-        
+
         $this->view->polls = Polls::find();
     }
-    
-    public function showAction( $poll_id )
+
+    public function showAction( $pollId )
     {
         $this->view->pick( "poll/show" );
-        
+
         $this->view->poll = Polls::findFirst(
             "WHERE id=:id",
             array(
-                "bind" => array("id" => $poll_id)
+                "bind" => array("id" => $pollId)
             )
         );
-        
+
         $this->view->options = PollsOptions::find(
             "WHERE polls_id=:poll_id ORDER BY number_votes DESC;",
             array(
-                "bind" => array("poll_id" => $poll_id)
+                "bind" => array("poll_id" => $pollId)
             )
         );
     }
-    
-    public function voteAction( $option_id )
+
+    public function voteAction( $optionId )
     {
         $option = PollsOptions::findFirst(
             "WHERE id=:id",
             array(
-                "bind" => array("id" => $option_id)
+                "bind" => array("id" => $optionId)
             )
         );
         $option->number_votes++;
         $option->save();
-        
-        $this->dispatcher->forward( array(
+
+        return $this->dispatcher->forward( array(
             "controller" => "poll",
             "action" => "show",
             "params" => array($option->polls_id)
         ) );
+    }
+
+    public function addAction( $pollId )
+    {
+        $this->view->pick( "poll/add" );
+
+        if ($this->request->isPost())
+        {
+            $option = new PollsOptions();
+            $option->polls_id = $pollId;
+            $option->name = $this->request->getPost("name");
+            $option->number_votes = 0;
+            $option->save();
+
+            return $this->dispatcher->forward( array(
+                "controller" => "poll",
+                "action" => "show",
+                "params" => array($option->polls_id)
+            ) );
+        }
     }
 }
