45a3c21df9cbaae4232393fb1bf413e70fe5fb73
sios13 <simon2@live.se>
2017-06-27 19:47:12 +0200

    404 status code is now set in action

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -12,12 +12,18 @@ class IndexController extends ControllerBase
         $this->tag->prependTitle( "" );
         $this->view->pick( "index/index" );
 
-        //$this->view->enableCache( array("key" => "index-cache", "lifetime" => 100, "level" => 5) );
+        // $this->view->enableCache( [
+        //     "key" => "index-cache",
+        //     "lifetime" => 100,
+        //     "level" => 5
+        // ] );
     }
 
     public function notFoundAction()
     {
         $this->tag->setTitle( "404 - Sidan finns inte" );
         $this->view->pick( "error/404" );
+
+        $this->response->setStatusCode( 404, "Not found" );
     }
 }
